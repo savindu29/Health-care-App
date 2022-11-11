406f3b0df21dff2fab54d69919e39548cbb53656
savindu29 <1savindupanagoda@gmail.com>
2022-11-11 21:03:31 +0530

    6

--- src/lk/ijse/healthcare/controller/DefaultFormController.java
+++ src/lk/ijse/healthcare/controller/DefaultFormController.java
@@ -1,4 +1,31 @@
 package lk.ijse.healthcare.controller;
 
+import javafx.scene.control.Label;
+import lk.ijse.healthcare.bo.BoFactory;
+import lk.ijse.healthcare.bo.BoTypes;
+import lk.ijse.healthcare.bo.custom.AppointmentBo;
+import lk.ijse.healthcare.bo.custom.DoctorBo;
+import lk.ijse.healthcare.bo.custom.PatientBo;
+
+import java.sql.SQLException;
+
 public class DefaultFormController {
+
+    public Label lblNumberOfAppointments;
+    public Label lblNumberOfDoctors;
+    public Label lblNumberOdPatients;
+    private DoctorBo boD = BoFactory.getInstance().getBo(BoTypes.DOCTOR);
+    private PatientBo boP= BoFactory.getInstance().getBo(BoTypes.PATIENT);
+    private AppointmentBo boA = BoFactory.getInstance().getBo(BoTypes.APPOINTMENT);
+    public void initialize(){
+        try {
+            lblNumberOdPatients.setText(boP.searchPatients("").size()+"");
+            lblNumberOfDoctors.setText(boD.searchDoctors("").size()+"");
+            lblNumberOfAppointments.setText(boA.searchAppointment("").size()+"");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
